6dd0ef0c12a795c6ca341cb7d306de948850e244
Matt Fellows <matt.fellows@onegeek.com.au>
2018-10-24 22:12:53 +1100

    fix(test): create pact example test on jest 7

--- pact/setup.js
+++ pact/setup.js
@@ -1,15 +1,17 @@
-const path = require('path');
-const PactWeb = require('@pact-foundation/pact-web').PactWeb;
+const path = require("path");
+const Pact = require("@pact-foundation/pact").Pact;
 
 global.port = 8080;
-global.provider = new PactWeb({
-    port: global.port,
-    log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
-    loglevel: 'debug',
-    dir: path.resolve(process.cwd(), 'pacts'),
-    spec: 2,
-    pactfileWriteMode: 'update',
-    consumer: 'react-consumer',
-    provider: 'node-provider',
-    host: '127.0.0.1'
+global.provider = new Pact({
+  port: global.port,
+  cors: true,
+  log: path.resolve(process.cwd(), "logs", "mockserver-integration.log"),
+  loglevel: "debug",
+  dir: path.resolve(process.cwd(), "pacts"),
+  spec: 2,
+  // TODO: set this back to update only if you know what you're doing! (for parallel stuff) :)
+  pactfileWriteMode: "overwrite",
+  consumer: "react-consumer",
+  provider: "node-provider",
+  host: "127.0.0.1"
 });
